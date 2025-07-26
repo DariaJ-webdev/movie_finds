552b8ce8ea2eae0965733bf39224569650c29d4d
Daria J. <frontendwebcraftx@gmail.com>
2025-07-26 19:24:26 -0400

    Update index.js
    had to change base_url to an https:// address

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // OMDb API Configuration
 const API_KEY = "f58252f"; 
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 // Placeholder image because they charge for posters
 const UNDRAW_MOVIE_WATCHING_IMAGE = "./undraw_movie.svg"; //
